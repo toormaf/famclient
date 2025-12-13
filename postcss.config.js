f8ffa6b75b33defca2a423950a47324265008b72
toormaf <toormaf@gmail.com>
2025-12-13 22:43:58 +0530

    Updated package.json

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,8 @@
 export default {
   plugins: {
-    tailwindcss: {},
+    tailwindcss: {
+      config: './config/tailwind.config.js'
+    },
     autoprefixer: {},
   },
 }
--- src/utils/index.ts
+++
@@ -1,5 +0,0 @@
-export { ApiMaker } from './ApiMaker';
-export { LRUCache } from './LRUCache';
-export { CookieManager } from './CookieManager';
-export * from './api';
-export * from '../types/api';
