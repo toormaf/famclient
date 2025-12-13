a2997873cd17fb1952f3c8d99d39dce4ccd637c3
toormaf <toormaf@gmail.com>
2025-12-13 22:59:07 +0530

    Updated main.tsx

--- config/vite.config.ts
+++ config/vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  envDir: path.resolve(__dirname, 'env'),
   server: {
     port: 5173,
   },
--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { ConfigProvider } from 'antd';
 import App from './App.tsx';
-import themeConfig from './theme/themeConfig.ts';
+import themeConfig from '../config/themeConfig';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
